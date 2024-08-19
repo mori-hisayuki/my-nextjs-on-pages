640ef889752bd47fd0fb1c151f4ea835d617145c
mori-hisayuki <h.mori0206@gmail.com>
2024-08-19 22:19:56 +0900

    feat: add upload logic to  UploadPage

--- src/app/photos/upload/page.tsx
+++ src/app/photos/upload/page.tsx
@@ -1,10 +1,24 @@
+import { getRequestContext } from "@cloudflare/next-on-pages";
+
 export const runtime = "edge";
 
 const UploadPage = () => {
+
+  // server action
+  const upload = async (formData: FormData) => {
+    "use server";
+    const photo = formData.get("photo") as File;
+    const bucket = getRequestContext().env.MY_BUCKET;
+
+    await bucket.put(photo.name, photo);
+
+    console.log("Uploaded", photo.name);
+  }
+
   return(
     <div className="p-4 space-y-2 min-h-screen">
       <h1 className="text-2xl font-bold">Upload</h1>
-      <form className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
+      <form action={upload} className="bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl">
         <input type="file" name="photo" required accept="image/*" />
         <button
           type="submit"
